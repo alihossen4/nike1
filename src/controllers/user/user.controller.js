dcd1331e4820be521dcaae1b46b1b1c4c9b86ad2
alihossen4 <mdahossen.dev@gmail.com>
2025-05-21 15:10:55 +0600

    feat: enhance signup logic to check for existing username and email

--- src/controllers/user/user.controller.js
+++ src/controllers/user/user.controller.js
@@ -1,13 +1,23 @@
 import { asyncHandler } from '../../utils/asyncHandler.js';
 import {User} from "../../models/user.model.js";
+import ApiError from '../../utils/apiError.js';
 // const asyncHandler = (fn) =>(req, res, next ) => Promise.resolve(fn(req, res, next)).catch(err => next(err))
 const signup = asyncHandler(async (req, res) => {
   console.log(req.body);
   const {username, name, email, password} = req.body;
-  const user = await User.create(req.body);
+  const userExist = await User.findOne({username});
+  if(userExist){
+    throw ApiError.badRequest({success: false, message: "User already exists"});
+  }
+  const emailExist = await User.find({email});
+  if(emailExist){
+    throw ApiError.badRequest({success: false, message: "Email already exists"});
+  }
+  const user = await User.create({ username, name, email, password});
+  // const user = await User.create(req.body);
   console.log(user);
   
-  return res.json( {user});
+  return res.status(200).json( {user});
 });
 
 export { signup };
